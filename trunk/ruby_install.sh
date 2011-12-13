81d48262948728760c20334c3c51d4af02a774f1
jrw32 <jrw32@dce.psu.edu>
2011-12-13 21:41:32 +0000

    Final version  of ruby installer
    git-svn-id: https://svn.ritchielab.psu.edu/svn/projects/visualization@1597 9e62ecf6-770d-0410-a9d7-e62a33e3d092

--- trunk/ruby_install.sh
+++ trunk/ruby_install.sh
@@ -7,7 +7,7 @@ RUBY_V=`ruby -v`
 # capture the exit value
 EV=$?
 
-if test $EV == 127; then
+if test $EV -eq 127; then
 	INSTALL_RUBY=1
 	RUBY_V="ruby-1.9.3-p0"
 	RUBY_MAJMIN="1.9"
@@ -18,33 +18,32 @@ else
 	RUBY_V=`echo $RUBY_V | sed "s/ruby \([0-9]\.[0-9]\.[0-9]\)p\([0-9]*\).*/ruby-\1-p\2/g"`
 fi
 
-if test $INSTALL_RUBY -eq 0; then
+if test $INSTALL_RUBY -eq 1; then
 	# download and install ruby
 	echo "Installing ruby v1.9.3p0..."
+	echo "Downloading $RUBY_V.tar.gz"
 	curl "$RUBY_WEBSITE/$RUBY_MAJMIN/$RUBY_V.tar.gz" > $RUBY_V.tar.gz
-	echo "Downloaded"
 	tar -xzf $RUBY_V.tar.gz
 	cd  $RUBY_V
 	# I'm assuming that you have access to the necessary development
 	# tools to compile ruby from source
-	./configure
-	YAML_INST = `make 2>&1 | grep "libyaml is missing"`
-	echo "YAML $YAML_INST"
-	exit 1
+	./configure >/dev/null
+	YAML_INST=`make 2>&1 | grep "libyaml is missing"`
 	if test !-z $YAML_INST; then
 		echo "LibYAML not found, installing..."
 		# We need to install libyaml first
+		echo "Downloading yaml-0.14.tar.gz"
 		curl http://pyyaml.org/download/libyaml/yaml-0.1.4.tar.gz > yaml-0.14.tar.gz
-		tar -xzf yaml-0.1.4.tar.gz
+		tar -xzf yaml-0.14.tar.gz
 		cd yaml-0.1.4
-		./configure 
-		make
-		make install
+		./configure >/dev/null
+		make >/dev/null
+		#make install >/dev/null
 		cd ..
-		make clean
-		make
+		make clean >/dev/null 2>&1
+		make >/dev/null 2>&1
 	fi
-	make install
+	#make install >/dev/null
 	cd ..
 fi
 
@@ -53,7 +52,7 @@ INSTALL_RMAGICK=0
 GEM_LIST=`gem list`
 EV=$?
 
-if test $EV == 127; then
+if test $EV -eq 127; then
 	INSTALL_GEMS=1
 fi
 
@@ -63,18 +62,20 @@ fi
 
 if test $INSTALL_GEMS -eq 1; then
 	echo "Installing RubyGems v1.8.12..."
+	echo "Downloading rubygems-1.8.12.tgz"
 	# download and install rubygems for the appropriate version here
 	curl http://production.cf.rubygems.org/rubygems/rubygems-1.8.12.tgz > rubygems-1.8.12.tgz
 	tar -xzf rubygems-1.8.12.tgz
 	cd rubygems-1.8.12
-	ruby setup.rb
+	#ruby setup.rb >/dev/null
 	cd ..	
 fi
 
 if test $INSTALL_RMAGICK -eq 1; then
 	echo "Installing RMagick..."
 	# Use rubygems to install rmagick
-	gem install rmagick
+	#gem install rmagick >/dev/null
 fi
 
+echo "All programs installed"
 #OK, you should be good to go now!
